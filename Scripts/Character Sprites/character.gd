e794e0cbeab1781657e827a877953f80d84b897d
cliveG <56766746+CliveGlitch@users.noreply.github.com>
2025-09-21 19:59:40 -0600

    test: More dialogue

--- Scripts/Character Sprites/character.gd
+++ Scripts/Character Sprites/character.gd
@@ -116,7 +116,6 @@ func _get_covered_rect() -> Rect2:
 func _on_text_start(_data) -> void:
 	# Dialogic2 character name must match the Node name
 	var character_who_will_talk = Dialogic.Text.get_character_name_parsed(_data.character)
-	var current_characer = dialogic_name
 	var isCharacterText = character_who_will_talk.to_upper() == dialogic_name.to_upper()
 	isTalking = isCharacterText
 	
